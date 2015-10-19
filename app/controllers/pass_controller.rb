67b32bebf8aedd5d6775ae354ce5a797e6634c2c
Dan W <dantech88@yahoo.com>
2015-10-19 12:52:15 +0000

    setup pass controller framework

--- app/controllers/pass_controller.rb
+++ app/controllers/pass_controller.rb
@@ -1,3 +1,23 @@
 class PassController < ApplicationController
   
+  #create pass, if by teacher, it is automatically verified
+  #if by student, student must request from class page and be in class
+  def create
+  
+  end
+  
+  #detailed view of pass
+  def view
+  
+  end
+  
+  #teacher verifies pass with their teacher_id, makes pass valid
+  def verify
+    
+  end
+  
+  #teacher with teacher_id_to verifies that the student arrived and destroys the pass
+  def destroy
+    
+  end
 end
