4212116d758cb8623930d4a0706bac55d88ff61c
Dan W <dantech88@yahoo.com>
2015-10-19 03:13:28 +0000

    added add and remove student

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -48,12 +48,58 @@ class CourseController < ApplicationController
   #teacher manually add student or through join code
   #create enrollment for user to add them
   def add_student
-    
+    #teacher added student
+    if params[:join_code] == nil or params[:join_code] = ""
+      @teacher = current_user
+      if @teacher.user_type == 2 or @teacher.user_type = 3
+        @course = Course.find_by(id: params[:course_id])
+        if @course and @course.teacher_id = @teacher.id
+          @student = User.find_by(id: params[:student_id])
+          if @student and (@student.enrollments.where(course_id: @course.id) == nil or @student.enrollments.where(course_id: @course.id) == []) and @student.id != @teacher.id
+            @enrollment = Enrollment.new
+            @enrollment.course_id = @course.id
+            @enrollment.user_id = @student.id
+            @enrollment.save
+            redirect_to "/courses/view/#{@course.id}"
+          else
+            redirect_to "/users/view/#{@teacher.id}"
+          end
+        else
+          redirect_to "/users/view/#{@teacher.id}"
+        end
+      else
+        redirect_to "/users/view/#{@teacher.id}"
+      end
+    #student joined with join code
+    else
+      
+    end
   end
   
   #teacher removes student
   def remove_student
-    
+    @teacher = current_user
+    if @teacher.user_type == 2 or @teacher.user_type = 3
+      @course = Course.find_by(id: params[:course_id])
+      if @course and @course.teacher_id = @teacher.id
+        @student = User.find_by(id: params[:student_id])
+        if @student and (@student.enrollments.where(course_id: @course.id) != nil or @student.enrollments.where(course_id: @course.id) != [])
+          @enrollment = Enrollment.find_by(course_id: @course.id, user_id: @student.id)
+          if @enrollment
+            @enrollment.destroy
+            redirect_to "/courses/edit/#{@course.id}"
+          else
+            redirect_to "/courses/edit/#{@course.id}"
+          end
+        else
+          redirect_to "/courses/edit/#{@course.id}"
+        end
+      else
+        redirect_to "/users/view/#{@teacher.id}"
+      end
+    else
+      redirect_to "/users/view/#{@teacher.id}"
+    end
   end
   
   #enable and generate join code
